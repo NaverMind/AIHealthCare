bef4a47f68ee1b2a449c9ae97ec6890460ddb8f0
Chi-Honey <necktop9711@gmail.com>
2022-11-20 17:44:27 +0900

    graph in 운동노트 complete

--- senior_fit_1/lib/screens/note.dart
+++ senior_fit_1/lib/screens/note.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:senior_fit_1/screens/for_chart.dart';
 
 class Note extends StatefulWidget {
   const Note({Key? key}) : super(key: key);
@@ -10,10 +11,124 @@ class Note extends StatefulWidget {
 class _MyPageState extends State<Note> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: double.infinity,
-      child: Center(child: Text('운동노트')),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('운동 노트'),
+        backgroundColor: Colors.white,
+        titleTextStyle: TextStyle(color: Colors.black, fontWeight: FontWeight.bold,fontSize: 23),
+        elevation: 0.0,
+      ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                alignment: Alignment.centerLeft,
+                child: Row(
+                  children: [
+                    SizedBox(width: 12,),
+                    Image.asset(
+                      'images/plank.png',
+                      height: 40,
+                      width: 40,
+                      fit: BoxFit.contain,
+                    ),
+                    Text(
+                      '플랭크',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              LineChartSample2(),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                alignment: Alignment.centerLeft,
+                child: Row(
+                  children: [
+                    SizedBox(width: 12,),
+                    Image.asset(
+                      'images/pushup.png',
+                      height: 40,
+                      width: 40,
+                      fit: BoxFit.contain,
+                    ),
+                    Text(
+                      '푸시업',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              LineChartSample2(),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                alignment: Alignment.centerLeft,
+                child: Row(
+                  children: [
+                    SizedBox(width: 12,),
+                    Image.asset(
+                      'images/npush.png',
+                      height: 40,
+                      width: 40,
+                      fit: BoxFit.contain,
+                    ),
+                    Text(
+                      '무릎 푸시업',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              LineChartSample2(),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                alignment: Alignment.centerLeft,
+                child: Row(
+                  children: [
+                    SizedBox(width: 12,),
+                    Image.asset(
+                      'images/man.png',
+                      height: 40,
+                      width: 40,
+                      fit: BoxFit.contain,
+                    ),
+                    Text(
+                      '사이드 크런치',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              LineChartSample2(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
